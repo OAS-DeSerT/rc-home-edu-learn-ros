900649e1b0f6f02576547a76a5fbd6a0c63da698
OAS-DeSerT <18057332690@sina.cn>
2021-12-15 16:43:17 +0800

    Update dance_arm.py

--- rchomeedu_arm/scripts/dance_arm.py
+++ rchomeedu_arm/scripts/dance_arm.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-    arm.py - move robot arm according to predefined gestures
+    dance_arm.py - move robot arm according to predefined gestures
 
 """
 
@@ -15,87 +15,87 @@ class Loop:
 
         rospy.Subscriber('/dance_arm', String, self.callback)
 
-	# publish command message to joints/servos of arm
-    	self.joint1 = rospy.Publisher('/waist_controller/command',Float64)
-	self.joint2 = rospy.Publisher('/shoulder_controller/command',Float64)
-    	self.joint3 = rospy.Publisher('/elbow_controller/command',Float64)
-    	self.joint4 = rospy.Publisher('/wrist_controller/command',Float64)
-	self.joint5 = rospy.Publisher('/hand_controller/command',Float64)
-	self.pos1 = Float64()
-    	self.pos2 = Float64()
-    	self.pos3 = Float64()
-    	self.pos4 = Float64()
-    	self.pos5 = Float64()
-	
-	# Initial gesture of robot arm
-	self.pos1 = 1.565
-	self.pos2 = 2.102
-	self.pos3 = -2.439
-	self.pos4 = -1.294
-	self.pos5 = 0.0
-	self.joint1.publish(self.pos1)
-	self.joint2.publish(self.pos2)
-	self.joint3.publish(self.pos3)
-	self.joint4.publish(self.pos4)
-	self.joint5.publish(self.pos5)
+        # publish command message to joints/servos of arm
+        self.joint1 = rospy.Publisher('/waist_controller/command', Float64, queue_size=10)
+        self.joint2 = rospy.Publisher('/shoulder_controller/command', Float64, queue_size=10)
+        self.joint3 = rospy.Publisher('/elbow_controller/command', Float64, queue_size=10)
+        self.joint4 = rospy.Publisher('/wrist_controller/command', Float64, queue_size=10)
+        self.joint5 = rospy.Publisher('/hand_controller/command', Float64, queue_size=10)
+        self.pos1 = Float64()
+        self.pos2 = Float64()
+        self.pos3 = Float64()
+        self.pos4 = Float64()
+        self.pos5 = Float64()
+
+        # Initial gesture of robot arm
+        self.pos1 = 0.0
+        self.pos2 = -1.86
+        self.pos3 = 2.44
+        self.pos4 = 1.0
+        self.pos5 = 0.0
+        self.joint1.publish(self.pos1)
+        self.joint2.publish(self.pos2)
+        self.joint3.publish(self.pos3)
+        self.joint4.publish(self.pos4)
+        self.joint5.publish(self.pos5)
 
     def callback(self, msg):
-        print msg.data
+        print(msg.data)
         if msg.data == "dance arm":        
             count = 0
-	    while (count < 1):
+            while (count < 1):
 
-		# gesture 1
-		self.pos1 = 0.559
-		self.pos2 = 0.215
-		self.pos3 = -1.508
-		self.pos4 = -0.496
-		self.pos5 = 0.0
-		self.joint1.publish(self.pos1)
-		self.joint2.publish(self.pos2)
-		self.joint3.publish(self.pos3)
-		self.joint4.publish(self.pos4)
-		self.joint5.publish(self.pos5)
-		rospy.sleep(2)
-		
-		# gesture 2
-		self.pos1 = 0.565
-		self.pos2 = -2.393
-		self.pos3 = -0.639
-		self.pos4 = 1.335
-		self.pos5 = 0.0
-		self.joint1.publish(self.pos1)
-		self.joint2.publish(self.pos2)
-		self.joint3.publish(self.pos3)
-		self.joint4.publish(self.pos4)
-		self.joint5.publish(self.pos5)
-		rospy.sleep(3)
+                # gesture 1
+                self.pos1 = -1.0
+                self.pos2 = -0.215
+                self.pos3 = 1.508
+                self.pos4 = 0.496
+                self.pos5 = 0.0
+                self.joint1.publish(self.pos1)
+                self.joint2.publish(self.pos2)
+                self.joint3.publish(self.pos3)
+                self.joint4.publish(self.pos4)
+                self.joint5.publish(self.pos5)
+                rospy.sleep(2)
 
-		# gesture 1
-		self.pos1 = 0.559
-		self.pos2 = 0.215
-		self.pos3 = -1.508
-		self.pos4 = -0.496
-		self.pos5 = 0.0
-		self.joint1.publish(self.pos1)
-		self.joint2.publish(self.pos2)
-		self.joint3.publish(self.pos3)
-		self.joint4.publish(self.pos4)
-		self.joint5.publish(self.pos5)
-		rospy.sleep(2)
+                # gesture 2
+                self.pos1 = -1.0
+                self.pos2 = 0.24
+                self.pos3 = 0.639
+                self.pos4 = -1.0
+                self.pos5 = 0.0
+                self.joint1.publish(self.pos1)
+                self.joint2.publish(self.pos2)
+                self.joint3.publish(self.pos3)
+                self.joint4.publish(self.pos4)
+                self.joint5.publish(self.pos5)
+                rospy.sleep(3)
 
-		# initial gesture
-		self.pos1 = 0.565
-		self.pos2 = 2.102
-		self.pos3 = -2.439
-		self.pos4 = -1.294
-		self.pos5 = 0.0
-		self.joint1.publish(self.pos1)
-		self.joint2.publish(self.pos2)
-		self.joint3.publish(self.pos3)
-		self.joint4.publish(self.pos4)
-		self.joint5.publish(self.pos5)
-		rospy.sleep(3)
+                # gesture 1
+                self.pos1 = -1.0
+                self.pos2 = -0.215
+                self.pos3 = 1.508
+                self.pos4 = 0.496
+                self.pos5 = 0.0
+                self.joint1.publish(self.pos1)
+                self.joint2.publish(self.pos2)
+                self.joint3.publish(self.pos3)
+                self.joint4.publish(self.pos4)
+                self.joint5.publish(self.pos5)
+                rospy.sleep(2)
+
+                # initial gesture
+                self.pos1 = -0.5
+                self.pos2 = -1.86
+                self.pos3 = 2.44
+                self.pos4 = 1.0
+                self.pos5 = 0.0
+                self.joint1.publish(self.pos1)
+                self.joint2.publish(self.pos2)
+                self.joint3.publish(self.pos3)
+                self.joint4.publish(self.pos4)
+                self.joint5.publish(self.pos5)
+                rospy.sleep(3)
 
                 count = count + 1
 
@@ -109,4 +109,3 @@ if __name__=="__main__":
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
-
