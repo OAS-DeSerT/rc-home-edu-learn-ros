639dc1ce06f947f299c6318688b57339591f2349
OAS-DeSerT <18057332690@sina.cn>
2021-12-15 16:39:00 +0800

    Update partybot.py

--- rchomeedu_apps/rchomeedu_partybot/scripts/partybot.py
+++ rchomeedu_apps/rchomeedu_partybot/scripts/partybot.py
@@ -45,17 +45,17 @@ class PartyBot:
         # self.soundhandle.say("Ready")
         
         rospy.loginfo("Ready, waiting for commands...")
-	self.soundhandle.say('Hello, I am PartyBot. What can I do for you?')
-	#rospy.sleep(2)
+        self.soundhandle.say('Hello, I am PartyBot. What can I do for you?')
+        #rospy.sleep(2)
 
         # Subscribe to the recognizer output and set the callback function
-        rospy.Subscriber('/lm_data', String, self.talkback)
+        rospy.Subscriber('recognizer/output', String, self.talkback)
 
-	self.dance_arm = rospy.Publisher("dance_arm", String, queue_size=10)
+        self.dance_arm = rospy.Publisher("dance_arm", String, queue_size=10)
 
         self.take_photo = rospy.Publisher("take_photo", String, queue_size=10)
 
-	self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
+        self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
 
     def control_follow(self, msg):
         rospy.wait_for_service('/turtlebot_follower/change_state')
@@ -72,92 +72,92 @@ class PartyBot:
         # call('rosrun sound_play say.py "montreal"', shell=True)
         # rospy.sleep(1)
 
-	if msg.data.find('INTRODUCE-YOURSELF')>-1:
-        	self.soundhandle.playWave(self.wavepath + "/R2D2a.wav")
-        	#rospy.sleep(1)
-		self.soundhandle.say("I heard you want me to introduce myself. I am PartyBot. I am a party robot to serve you and have fun.")
-		#rospy.sleep(10) 
-	elif msg.data.find('HOW-OLD-ARE-YOU')>-1:
-        	self.soundhandle.playWave(self.wavepath + "/R2D2a.wav")
-        	#rospy.sleep(1)
-		self.soundhandle.say("I heard you ask about my age. I am five years old.")
-		#rospy.sleep(5) 
-	elif msg.data.find('FOLLOW-ME')>-1:
-        	self.soundhandle.playWave(self.wavepath + "/R2D2a.wav")
-        	#rospy.sleep(1)
-		self.soundhandle.say("OK. I will start follow you.")
-		#rospy.sleep(5)
-                self.control_follow(1)
-	elif msg.data.find('STOP-FOLLOW')>-1:
-        	self.soundhandle.playWave(self.wavepath + "/R2D2a.wav")
-        	#rospy.sleep(1)
-		self.soundhandle.say("OK. I will stop follow you.")
-		#rospy.sleep(5)
-                self.control_follow(0)
-	elif msg.data.find('ARE-YOU-FROM')>-1:
-        	self.soundhandle.playWave(self.wavepath + "/R2D2a.wav")
-        	#rospy.sleep(1)
-		self.soundhandle.say("I heard you ask about my hometown. I am from China.")
-		#rospy.sleep(5)
-	elif msg.data.find('CAN-YOU-DO')>-1:
-        	self.soundhandle.playWave(self.wavepath + "/R2D2a.wav")
-        	#rospy.sleep(1)
-		self.soundhandle.say("I heard you ask me what can I do? I am a home robot. I am good at singing and dancing. I tell funny jokes and I take great photos of people")
-		#rospy.sleep(5)
-	elif msg.data.find('TELL-A-FUNNY-JOKE')>-1:
-        	self.soundhandle.playWave(self.wavepath + "/R2D2a.wav")
-        	#rospy.sleep(1)
-		self.soundhandle.say("You want to hear a joke? What is orange and sounds like a parrot? Erm, It is a carrot. Ha ha ha")
-		#rospy.sleep(8)
-	elif msg.data.find('SING-AND-DANCE')>-1:
-        	self.soundhandle.playWave(self.wavepath + "/R2D2a.wav")
-        	#rospy.sleep(1)
-		self.soundhandle.say("You want me to sing and dance? sure. let me show you")
-		#rospy.sleep(5)
-                self.dance_arm.publish('dance arm')      	
-		self.soundhandle.playWave(self.wavepath + "/swtheme.wav", blocking=False)
-		#rospy.sleep(1) 
-		# Dancing
-		# create two different Twist() variables.  One for moving forward.  One for turning 45 degrees.
-		# let's go forward at 0.2 m/s
-		move_cmd = Twist()
-		move_cmd.linear.x = 0.2
-		# by default angular.z is 0 so setting this isn't required
-	        #let's turn at 45 deg/s
-        	turn_cmd = Twist()
-        	turn_cmd.linear.x = 0
-		turn_cmd.angular.z = radians(90); #45 deg/s in radians/s
-        	turn_cmd2 = Twist()
-        	turn_cmd2.linear.x = 0
-		turn_cmd2.angular.z = radians(-90); #45 deg/s in radians/s
-		# turn 90 degrees
-		rospy.loginfo("Turning")
-		for x in range(0,5):
-			self.cmd_vel.publish(turn_cmd)
-			# r.sleep()
-			rospy.sleep(1)
-			self.cmd_vel.publish(turn_cmd2)
-			rospy.sleep(1) 
-		rospy.sleep(6)
-	elif msg.data.find('TAKE-A-PHOTO')>-1:
-        	self.soundhandle.playWave(self.wavepath + "/R2D2a.wav")
-        	#rospy.sleep(1)
-		#call('rosrun image_view image_view image:=/camera_top/rgb/image_raw', shell=False)
-		#rospy.sleep(1)
-		self.soundhandle.say("You want to take a photo? Ok, get ready. One, two, three, say cheese")
-                self.take_photo.publish('take photo')
-                #call('rosrun rchomeedu_vision take_photo.py', shell=True)
-		#rospy.sleep(6)
-                #call('rosrun image_view image_saver image:=/camera_top/rgb/image_raw _save_all_image:=false _filename_format:=foo.jpg __name:=image_saver', shell=True)
-		#call('rosservice call /image_saver/save', shell=True)
-		#rospy.sleep(6)
-	#else: self.soundhandle.say("Sorry, I cannot hear you clearly. Please say again.")
-	else: rospy.sleep(3)
-        
+        if msg.data.find('introduce-yourself')>-1:
+            self.soundhandle.playWave(self.wavepath + "/R2D2a.wav")
+            #rospy.sleep(1)
+            self.soundhandle.say("I heard you want me to introduce myself. I am PartyBot. I am a party robot to serve you and have fun.")
+            #rospy.sleep(10) 
+        elif msg.data.find('how-old-are-you')>-1:
+            self.soundhandle.playWave(self.wavepath + "/R2D2a.wav")
+            #rospy.sleep(1)
+            self.soundhandle.say("I heard you ask about my age. I am five years old.")
+            #rospy.sleep(5) 
+        elif msg.data.find('follow-me')>-1:
+            self.soundhandle.playWave(self.wavepath + "/R2D2a.wav")
+            #rospy.sleep(1)
+            self.soundhandle.say("OK. I will start follow you.")
+            #rospy.sleep(5)
+            self.control_follow(1)
+        elif msg.data.find('stop-follow')>-1:
+            self.soundhandle.playWave(self.wavepath + "/R2D2a.wav")
+            #rospy.sleep(1)
+            self.soundhandle.say("OK. I will stop follow you.")
+            #rospy.sleep(5)
+            self.control_follow(0)
+        elif msg.data.find('are-you-from')>-1:
+            self.soundhandle.playWave(self.wavepath + "/R2D2a.wav")
+            #rospy.sleep(1)
+            self.soundhandle.say("I heard you ask about my hometown. I am from China.")
+            #rospy.sleep(5)
+        elif msg.data.find('can-you-do')>-1:
+            self.soundhandle.playWave(self.wavepath + "/R2D2a.wav")
+            #rospy.sleep(1)
+            self.soundhandle.say("I heard you ask me what can I do? I am a home robot. I am good at singing and dancing. I tell funny jokes and I take great photos of people")
+            #rospy.sleep(5)
+        elif msg.data.find('tell-a-funny-joke')>-1:
+            self.soundhandle.playWave(self.wavepath + "/R2D2a.wav")
+            #rospy.sleep(1)
+            self.soundhandle.say("You want to hear a joke? What is orange and sounds like a parrot? Erm, It is a carrot. Ha ha ha")
+            #rospy.sleep(8)
+        elif msg.data.find('sing-and-dance')>-1:
+            self.soundhandle.playWave(self.wavepath + "/R2D2a.wav")
+            #rospy.sleep(1)
+            self.soundhandle.say("You want me to sing and dance? sure. let me show you")
+            #rospy.sleep(5)
+            self.dance_arm.publish('dance arm')      	
+            self.soundhandle.playWave(self.wavepath + "/swtheme.wav", blocking=False)
+            #rospy.sleep(1) 
+            # Dancing
+            # create two different Twist() variables.  One for moving forward.  One for turning 45 degrees.
+            # let's go forward at 0.2 m/s
+            move_cmd = Twist()
+            move_cmd.linear.x = 0.2
+            # by default angular.z is 0 so setting this isn't required
+            #let's turn at 45 deg/s
+            turn_cmd = Twist()
+            turn_cmd.linear.x = 0
+            turn_cmd.angular.z = radians(90); #45 deg/s in radians/s
+            turn_cmd2 = Twist()
+            turn_cmd2.linear.x = 0
+            turn_cmd2.angular.z = radians(-90); #45 deg/s in radians/s
+            # turn 90 degrees
+            rospy.loginfo("Turning")
+            for x in range(0,5):
+                self.cmd_vel.publish(turn_cmd)
+                # r.sleep()
+                rospy.sleep(1)
+                self.cmd_vel.publish(turn_cmd2)
+                rospy.sleep(1) 
+            rospy.sleep(6)
+        elif msg.data.find('take-a-photo')>-1:
+            self.soundhandle.playWave(self.wavepath + "/R2D2a.wav")
+            #rospy.sleep(1)
+            #call('rosrun image_view image_view image:=/camera_top/rgb/image_raw', shell=False)
+            #rospy.sleep(1)
+            self.soundhandle.say("You want to take a photo? Ok, get ready. One, two, three, say cheese")
+            self.take_photo.publish('take photo')
+            #call('rosrun rchomeedu_vision take_photo.py', shell=True)
+            #rospy.sleep(6)
+            #call('rosrun image_view image_saver image:=/camera_top/rgb/image_raw _save_all_image:=false _filename_format:=foo.jpg __name:=image_saver', shell=True)
+            #call('rosservice call /image_saver/save', shell=True)
+            #rospy.sleep(6)
+        #else: self.soundhandle.say("Sorry, I cannot hear you clearly. Please say again.")
+        else: rospy.sleep(3)
+
         # Uncomment to play one of the built-in sounds
         #rospy.sleep(2)
         #self.soundhandle.play(5)
-        
+
         # Uncomment to play a wave file
         #rospy.sleep(2)
         #self.soundhandle.playWave(self.wavepath + "/R2D2a.wav")
